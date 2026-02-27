67760d9c11d2b4537f927ac04553a4def64559c0
Thorsten Koechling <koechling@labmed.de>
2026-02-27 17:06:36 +0100

    WIP: disease_db -> web form integration

--- disease_web/src/app/disease/create/create.component.ts
+++ disease_web/src/app/disease/create/create.component.ts
@@ -46,7 +46,7 @@ export class CreateComponent {
   ];
   disease = {
     //name: '',
-    name: 'Morbus Segelohren',
+    name: 'Hereditäre Sphärozytose',
     general_info: '',
     associated_panels: [],
     filter_info: '',
@@ -60,19 +60,67 @@ export class CreateComponent {
 
   // festival de creatividad!!
   // Initialize the form with the instructional text, user must overwrite it 
-  createForm = this.fb.group({
+  // move to a separate file, e.g. instructions.ts, and import it here, to keep the component clean
+
+  // do some shit here to get the associated panels in correct order and construct
+  // a string that is then passed to the template, formatted like the default
+  // one below (be sure to write-protect the field):
+  instructionsAssociatedPanels: string = "1) Core-Panel\n2) Erweitertes Panel\n3) ..."; 
+  instructionsFilterInfo: string = "Bitte sehr kurz fassen: nur Stichworte,\n\
+z.B. \u03B1LELY, \u03B1LEPRA, HBB ohne Filter, etc.\nDetails haben unten Platz!";
+  instructionsAnalysisFeatures: string = "Core-Panel\n-...\n-...\nErweitertes Panel\
+\n-...\n-...";
+  instructionsReportInfo: string = "Hier wäre Platz für die Allgemeine Info aus den ELO-Vorlagen";
+  instructionsReportText: string = "z.B. Extrasätze wie bei unauffälligen Befunden bzgl. hEDS bei EDS";
+  instructionsReportTech: string = "Ausnahmen darlegen";
+  instructionsGeneralInfo: string = "Genetische / molekulargenetische Auffälligkeiten\n\
+    -- Chromosomale Aberrationen, häufige pathogene Varianten, Pathomechanismus,\n\
+        Kopienzahlveränderungen (CNVs), Miochondriale Veränderungen\n\n\
+Laborchemische Auffälligkeiten\n\
+    -- Klinische Chemie, Endokrinologie etc.\n\n\
+Hämatologische Auffälligkeiten\n\
+    -- Blutbild\n\n\
+Immunologische / serologische Auffälligkeiten\n\
+    -- Antikörper, Immunglobuline etc.\n\n\
+Funktionelle Auffälligkeiten\n\
+    -- Kardiologisch, pulmonal, neurologisch, gastrointestinal etc.\n\n\
+Morphologische Auffälligkeiten\n\
+    -- Kraniofaziale Besonderheiten, Skelettanomalien, Organfehlbildungen, Wachstumsauffälligkeiten\n\n\
+Neurologische / neuropsychiatrische Auffälligkeiten\n\
+    -- Motorik, Sensibilität, Kognition & Verhalten etc.\n\n\
+Auffälligketen in bildgebenden Analysen\n\
+    -- Sonographisch, radiologisch, MRT etc.\n\n\
+Histopathologische Auffälligkeiten\n\
+    -- Biopsie, Zellatypien etc.\n";
+
+
+  initFormValues = {
+    name: this.disease.name,
+    general_info: this.instructionsGeneralInfo,
+    associated_panels: this.instructionsAssociatedPanels,
+    filter_info: this.instructionsFilterInfo,
+    analysis_features: this.instructionsAnalysisFeatures,
+    report_info: this.instructionsReportInfo,
+    report_text: this.instructionsReportText,
+    report_tech: this.instructionsReportTech
+  };
+
+  createForm: FormGroup = this.fb.group(this.initFormValues);
+
+  XXX_createForm = this.fb.group({
     name: [{value: '', disabled: false}, Validators.required],
     general_info: [''],
     associated_panels: [[]],
     filter_info: [''],
-    analysis_features: ['BLAHLABHllllasdfafasdfadsf\nBLAAAAHH!!!!'],
+    analysis_features: [''],
     report_info: [''],
     report_text: [''],
     report_tech: ['']
   });
 
-  filterInfoPlaceholder: string = "Bitte sehr kurz fassen: nur Stichworte, z.B. @LELY, @LEPRA, HBB ohne Filter, etc. Details haben unten Platz!";
-  analysisFeaturesPlaceholder: string = "Besonderheiten zu den verschiedenen Panels";
+  // placeholders are not suitable, use default text entries instead, see above
+  #filterInfoPlaceholder: string = "Bitte sehr kurz fassen: nur Stichworte, z.B. @LELY, @LEPRA, HBB ohne Filter, etc. Details haben unten Platz!";
+  #analysisFeaturesPlaceholder: string = "Besonderheiten zu den verschiedenen Panels";
 
   popupAnalysisFeaturesHelp() {
     console.log("Help for analysis features");
@@ -92,6 +140,7 @@ export class CreateComponent {
       report_text: [''],
       report_tech: ['']
     });
+    console.log(this.instructionsFilterInfo)
   }
 
   showPanelsTable: boolean = true;
